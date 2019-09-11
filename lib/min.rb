84e8ee5581ef05fefc55ff3e547335768f170759
Bruce Ruffin <bruffin1221@gmail.com>
2019-09-11 13:54:26 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -9,5 +9,13 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  #code your solution here!
+  store=[]
+  groceries = {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+  }
+  store<<groceries.values
+  store.flatten.sort.first
 end
--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,13 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+  store=[]
+  groceries = {
+    dairy: ["milk", "yogurt", "cheese"],
+    vegetable: ["carrots", "broccoli", "cucumbers"],
+    meat: ["chicken", "steak", "salmon"],
+    grains: ["rice", "pasta"]
+  }
+  store<<groceries.values
+  store.flatten
 end
