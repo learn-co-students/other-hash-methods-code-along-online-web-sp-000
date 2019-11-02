9be501e2569ebcaee5efa9b9bb4d2ef130558b0c
Chad Lengefeld <chadlengefeld@gmail.com>
2019-11-02 20:15:07 +0000

    Done.

--- lib/min.rb
+++ lib/min.rb
@@ -9,5 +9,7 @@ require 'pry'
 # }
 
 def get_the_min(groceries)
-  #code your solution here!
+
+groceries.values.flatten.min
+#returns the values, into one array, then finds the min value (a-z, 0-1)
 end
