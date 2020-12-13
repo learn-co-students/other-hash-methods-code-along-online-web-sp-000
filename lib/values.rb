ee710154529d5bf9f490437fe1ffeb7f035ad89d
Hannah Malm <hannahemalm616@gmail.com>
2020-12-13 23:38:38 +0000

    Done.

--- lib/values.rb
+++ lib/values.rb
@@ -9,5 +9,7 @@ require 'pry'
 # }
 
 def get_the_values(groceries)
-  #code your solution here!
+ groceries.values.flatten
 end
+
+#Using .values.flatten will return only the values without their keys in a one-dimensional array 
